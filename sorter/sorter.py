cffb2417de3ef616f5ee90155142d5c43a1fa460
Michal Krzyszton <michal.krzyszton@microsoft.wsei.edu.pl>
2022-05-21 17:37:04 +0200

    initial commit

---
+++ sorter/sorter.py
@@ -0,0 +1,25 @@
+from krita import *
+from datetime import date
+import os
+
+today = date.today()
+today_format = today.strftime("%d-%m-%Y")
+
+date_dir = f"{today_format}"
+parent_dir = "C:/Users/user/Desktop/Tree/"
+path = os.path.join(parent_dir, date_dir)
+file_exist = os.path.exists(path)
+
+
+def save():
+    currentDocument = Krita.instance().activeDocument()
+    file_name = Krita.instance().activeDocument().name()
+    currentDocument.saveAs(f"{path}/{file_name}")
+
+
+def execute():
+    if file_exist:
+        save()
+    else:
+        os.mkdir(path)
+        save()
